44d8f39d26d11e3a10ba395d45947a830618ac00
dnovak_home <dnovak@student.42.fr>
2024-10-03 15:25:19 +0200

    Basic setup of repository and project.

---
+++ unity_test/test_minitalk.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_minitalk.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/29 20:49:33 by dnovak            #+#    #+#             */
+/*   Updated: 2024/10/03 15:11:07 by dnovak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test_minitalk.h"
+
+void	setUp(void)
+{
+}
+
+void	tearDown(void)
+{
+}
+
+static void	test_input_regularInput(void)
+{
+}
+
+int	main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(test_input_regularInput);
+	return (UNITY_END());
+}
