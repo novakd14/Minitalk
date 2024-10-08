6c231fde7c2dd8a9c5e57bf195a658859382792a
dnovak_home <dnovak@student.42.fr>
2024-10-08 15:06:17 +0200

    First working version with timestamps on client side. Possible to use for evaluation. For extra info check README

--- Minitalk/server.h
+++ Minitalk/server.h
@@ -6,7 +6,20 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:10:00 by dnovak            #+#    #+#             */
-/*   Updated: 2024/10/03 15:19:33 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/10/08 12:22:27 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SERVER_H
+# define SERVER_H
+
+# define _POSIX_C_SOURCE 199309L
+
+# include "libft/libft.h"
+# include "test.h" //DELETE FOR EVAL
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+#endif // SERVER_H
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Minitalk
+
 The purpose of this project is to code a small data exchange program  using UNIX signals.
+
+# Notes
+
+## v1
+
++ Sending and recieving signals using ASCII encoding
++ Need to add:
+	+ proper queueing of signals
+	+ printing message after, **NOT** during recieving it (send size of message first)
+	+ **REMOVE** timestamps (leaving them in right now just for info)
+	+ [**NO BONUS!** - for bonus use sa_sigaction to get pid of calling process]
