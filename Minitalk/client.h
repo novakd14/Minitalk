779f800d2341cf0628773f26a0e8a33da057832d
dnovak_home <dnovak@student.42.fr>
2024-10-08 15:06:17 +0200

    First working version with timestamps on client side. Possible to use for evaluation. For extra info check README

--- Minitalk/client.h
+++ Minitalk/client.h
@@ -6,7 +6,19 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:10:00 by dnovak            #+#    #+#             */
-/*   Updated: 2024/10/03 15:19:20 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/10/08 13:19:00 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLIENT_H
+# define CLIENT_H
+
+# define _DEFAULT_SOURCE
+
+# include "libft/libft.h"
+# include "test.h"
+# include <signal.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+#endif // CLIENT_H
--- Minitalk/main_server.c
+++ Minitalk/main_server.c
@@ -6,7 +6,60 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:09:17 by dnovak            #+#    #+#             */
-/*   Updated: 2024/10/03 15:15:07 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/10/08 14:49:37 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "server.h"
+
+char		g_message[2];
+
+static void	signal_handler(int signum)
+{
+	if (signum == SIGUSR1)
+	{
+		g_message[0] = g_message[0] * 2 + 1;
+		g_message[1]++;
+	}
+	else if (signum == SIGUSR2)
+	{
+		g_message[0] = g_message[0] * 2;
+		g_message[1]++;
+	}
+}
+
+static void	set_signal(void)
+{
+	struct sigaction	action;
+	sigset_t			mask;
+
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGUSR1);
+	sigaddset(&mask, SIGUSR2);
+	ft_bzero(&action, sizeof(action));
+	action.sa_handler = &signal_handler;
+	action.sa_mask = mask;
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
+}
+
+int	main(void)
+{
+	ft_bzero(&g_message, sizeof(g_message));
+	set_signal();
+	ft_printf("%i\n", getpid());
+	while (1)
+	{
+		pause();
+		if (g_message[1] >= 8)
+		{
+			if (g_message[0] == 0)
+			{
+				write(1, "\n", 1);
+			}
+			write(1, g_message, 1);
+			g_message[0] = 0;
+			g_message[1] = 0;
+		}
+	}
+}
