dd1eea145b642ca26e480d07d6c7e086ddfe5d9e
dnovak_home <dnovak@student.42.fr>
2024-11-04 19:03:20 +0100

    v1.2 Reworked the mandatory part. Simplified signal handler.

--- Minitalk/main_client.c
+++ Minitalk/main_client.c
@@ -6,64 +6,89 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:09:17 by dnovak            #+#    #+#             */
-/*   Updated: 2024/10/09 14:35:26 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/11/04 12:58:08 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
-static int	ft_isnumber(char *num)
+static t_bool	ft_ispid(char *num)
 {
-	if (num == NULL || *num == 0)
-		return (0);
-	while (*num)
+	size_t	i;
+
+	if (*num == '\0')
+		return (FALSE);
+	i = 0;
+	while (*(num + i) != '\0')
 	{
-		if (!ft_isdigit(*num))
-			return (0);
-		num++;
+		if (ft_isdigit(*(num + i)) == FALSE)
+			return (FALSE);
+		i++;
 	}
-	return (1);
+	return (TRUE);
 }
 
-static void	send_message_size(int pid, int size)
+static void	send_message_size(pid_t pid, size_t size)
 {
-	int	bit_index;
+	size_t	num;
+	int		bit_pos;
+	int		byte;
+	size_t	bit_check;
 
-	bit_index = 0;
-	while (bit_index++ < (int)sizeof(int) * 8)
+	num = size;
+	bit_pos = 0;
+	while (num != 0)
 	{
-		if (size & (1 << (sizeof(int) * 8 - 1)))
-			kill(pid, SIGUSR1);
+		num >>= 1;
+		bit_pos += 1;
+	}
+	byte = (bit_pos - 1) / 8;
+	bit_check = (size_t)1 << ((byte + 1) * 8 - 1);
+	while (byte-- > 0)
+		kill_with_check(pid, SIGNAL1);
+	kill_with_check(pid, SIGNAL0);
+	while (bit_check != 0)
+	{
+		if ((size & bit_check) == 0)
+			kill_with_check(pid, SIGNAL0);
 		else
-			kill(pid, SIGUSR2);
-		size = size << 1;
-		usleep(500);
+			kill_with_check(pid, SIGNAL1);
+		bit_check >>= 1;
+	}
+}
+
+static void	send_message(int pid, char *mess)
+{
+	int	i;
+	int	bit_check;
+
+	i = 0;
+	while (*(mess + i) != '\0')
+	{
+		bit_check = 1 << 7;
+		while (bit_check != 0)
+		{
+			if ((*(mess + i) & bit_check) == 0)
+				kill_with_check(pid, SIGNAL0);
+			else
+				kill_with_check(pid, SIGNAL1);
+			bit_check >>= 1;
+		}
+		i++;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	int	pid;
-	int	bit_index;
+	pid_t	pid;
 
 	if (argc != 3)
-		return (1);
-	if (!ft_isnumber(argv[1]))
-		return (1);
+		exit_message(EXIT_FAILURE,
+			"ERROR: Wrong number of parameters.\n Use: ./client PID message\n");
+	if (ft_ispid(argv[1]) == FALSE)
+		exit_message(EXIT_FAILURE, "ERROR: Pass a valid PID number.\n");
 	pid = ft_atoi(argv[1]);
 	send_message_size(pid, ft_strlen(argv[2]));
-	while (*(argv[2]) != 0)
-	{
-		bit_index = 8;
-		while (bit_index-- > 0)
-		{
-			if (*(argv[2]) & 128)
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			*(argv[2]) = *(argv[2]) << 1;
-			usleep(500);
-		}
-		argv[2]++;
-	}
+	send_message(pid, argv[2]);
+	return (EXIT_SUCCESS);
 }
--- Minitalk/server.h
+++ Minitalk/server.h
@@ -6,33 +6,54 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:10:00 by dnovak            #+#    #+#             */
-/*   Updated: 2024/11/01 12:33:40 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/11/04 18:54:57 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_H
 # define SERVER_H
 
-# define _POSIX_C_SOURCE 199309L
+# ifndef _POSIX_C_SOURCE
+#  define _POSIX_C_SOURCE 199309L
+# endif
 
-# include "libft/libft.h"
-# include <signal.h>
-# include <stdlib.h>
-# include <sys/types.h>
-# include <unistd.h>
+# include "libft/libft.h" // ft_strlen(), ft_printf(), ft_calloc(), ft_bzero()
+							//-> <stdlib.h> exit(), free(), EXIT_*
+							//-> <unistd.h> get_pid(), write(), sleep(),
+# include <signal.h>      // kill(), sigaction(), SIGUSR*
+# include <sys/types.h>   // pid_t
+
+typedef enum e_signal
+{
+	SIGNAL0 = SIGUSR1,
+	SIGNAL1 = SIGUSR2,
+}					t_sig;
 
 typedef enum e_bool
 {
 	FALSE,
-	TRUE
-}			t_bool;
+	TRUE,
+}					t_bool;
+
+typedef enum e_status
+{
+	WAITING,
+	SIZE_PROP,
+	SIZE,
+	MESSAGE,
+}					t_status;
 
 typedef struct s_message
 {
-	int		size;
-	char	*message;
-	int		bite;
-	t_bool	recieving;
-}			t_message;
+	t_status		status;
+	size_t			size;
+	int				size_prop;
+	unsigned char	bytes[2];
+	int				byte_num;
+	size_t			bit_num;
+}					t_message;
+
+void				recieve_message(void);
+void				exit_message(int status, char *message);
 
 #endif // SERVER_H
