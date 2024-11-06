0e6d503cef22c410b3d7335c38e0bdc539a1e5c8
dnovak_home <dnovak@student.42.fr>
2024-11-06 02:34:08 +0100

    Trying bonus version. Working for sending message size reliably.

--- Minitalk/client.h
+++ Minitalk/client.h
@@ -6,19 +6,21 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:10:00 by dnovak            #+#    #+#             */
-/*   Updated: 2024/11/04 12:59:59 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/11/06 01:29:17 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLIENT_H
 # define CLIENT_H
 
+// FOR: usleep()
 # ifndef _DEFAULT_SOURCE
 #  define _DEFAULT_SOURCE
 # endif
 
-# ifndef WAIT_TIME
-#  define WAIT_TIME 300
+// FOR: signals
+# ifndef _POSIX_C_SOURCE
+#  define _POSIX_C_SOURCE
 # endif
 
 # include "libft/libft.h" // ft_isdigit(), ft_atoi(), ft_strlen()
@@ -27,19 +29,45 @@
 # include <signal.h>      // kill(), SIGUSR1, SIGUSR2
 # include <sys/types.h>   // pid_t
 
+# ifndef WAIT_TIME
+#  define WAIT_TIME 500000
+# endif
+
+# ifndef TIME_STEP
+#  define TIME_STEP 500
+# endif
+
 typedef enum e_signal
 {
 	SIGNAL0 = SIGUSR1,
 	SIGNAL1 = SIGUSR2,
-}		t_sig;
+	SIGNAL_SENDING = SIGUSR1,
+	SIGNAL_FINISH = SIGUSR2,
+}					t_sig;
+
+typedef enum e_status
+{
+	WAITING,
+	RECIEVED,
+	FINISH,
+}					t_status;
 
 typedef enum e_bool
 {
 	FALSE = 0,
 	TRUE = 1,
-}		t_bool;
+}					t_bool;
 
-void	exit_message(int status, char *message);
-void	kill_with_check(pid_t pid, int signal);
+typedef struct s_message
+{
+	sig_atomic_t	confirm;
+	sig_atomic_t	used;
+
+}					t_message;
+
+t_bool				ft_ispid(char *num);
+void				exit_message(int status, char *message);
+// void				advanced_pause(void);
+void				kill_with_check(pid_t pid, int signal, int status);
 
 #endif // CLIENT_H
--- Minitalk/utils_client.c
+++ Minitalk/utils_client.c
@@ -6,11 +6,29 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 12:20:26 by dnovak            #+#    #+#             */
-/*   Updated: 2024/11/04 12:58:12 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/11/06 02:26:10 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
+
+extern sig_atomic_t	g_status;
+
+t_bool	ft_ispid(char *num)
+{
+	size_t	i;
+
+	if (*num == '\0')
+		return (FALSE);
+	i = 0;
+	while (*(num + i) != '\0')
+	{
+		if (ft_isdigit(*(num + i)) == FALSE)
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
 
 void	exit_message(int status, char *message)
 {
@@ -24,10 +42,38 @@ void	exit_message(int status, char *message)
 	exit(status);
 }
 
-void	kill_with_check(pid_t pid, int signal)
+// void	advanced_pause(void)
+// {
+// 	if (usleep(WAIT_TIME) != -1)
+// 		ft_printf("NOTE: Waited for the whole usleep.\n");
+// 	if (g_mess.used == TRUE)
+// 		exit_message(EXIT_FAILURE,
+// 			"ERROR: Did not recieve confirmation from server.\n");
+// 	g_mess.used = TRUE;
+// }
+
+void	kill_with_check(pid_t pid, int signal, int status)
 {
+	int	time;
+
+	if (signal == SIGNAL0)  // TEMP
+		write(1, "0\n", 2); // TEMP
+	else                    // TEMP
+		write(1, "1\n", 2); // TEMP
 	if (kill(pid, signal) == -1)
 		exit_message(EXIT_FAILURE,
 			"ERROR: Could not send message. Signal was not recieved.\n");
-	usleep(WAIT_TIME);
+	time = 0;
+	while (g_status == WAITING)
+	{
+		if (time > WAIT_TIME)
+			exit_message(EXIT_FAILURE,
+				"ERROR: Did not recieve confirmation from server.\n");
+		time += TIME_STEP;
+		usleep(TIME_STEP);
+	}
+	if (g_status != status)
+		exit_message(EXIT_FAILURE,
+			"ERROR: Server did not recieve full message.\n");
+	g_status = WAITING;
 }
